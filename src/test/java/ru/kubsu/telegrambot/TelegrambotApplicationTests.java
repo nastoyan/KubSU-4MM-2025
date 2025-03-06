e0681156e68743a9e3e79fd8e6d72473afb98c70
Anastasia Ryzhenkova <anastasia.ryhenkova@mail.ru>
2025-03-06 22:38:49 +0300

    Spring Initializer

---
+++ src/test/java/ru/kubsu/telegrambot/TelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kubsu.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
